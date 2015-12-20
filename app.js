8d888b50d043f8d41db8f9e378d880d6156c7886
Athena Pelton <athenapelton@Athenas-MacBook.local>
2015-12-20 14:39:31 -0600

    added index page!

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
+//included libraries
 var express = require('express');
+
+//server configuration
 var app = express();
 
+app.set('view engine', 'ejs');
+
+
+//endpoints
 app.get('/', function (req, res) {
-  console.log('We got called!!!  We get to do something!');
-  res.send('Hello sweet chuck');
+  res.render('index', { name: 'Darryl' } );
 });
 
+app.get('/two', function (req, res) {
+  res.send('<html><body><h1>New Page</h1></body></html>');
+});
+
+//server startup
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
